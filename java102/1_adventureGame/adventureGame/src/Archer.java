7e674c8e81e15e926efb4f556af6ac2a87fd235d
Ceyda İncekara <143065808+ceyda-incekara@users.noreply.github.com>
2023-09-10 16:38:52 +0300

    Delete java102/1_adventureGame directory

--- java102/1_adventureGame/adventureGame/src/Archer.java
+++
@@ -1,5 +0,0 @@
-public class Archer extends GameChar {
-    public Archer() {
-        super(3,"Okçu\t",7, 18, 20);
-    }
-}
--- java102/1_adventureGame/adventureGame/src/Snake.java
+++
@@ -1,5 +0,0 @@
-public class Snake extends Obstacle{
-    public Snake() {
-        super(4,"Yılan", Player.SnakeFirstDamage(), 12 , 0);
-    }
-}
